7ad00bd74107a2dc05701a850480333dec8d1a13
hzliubenlong <Lbl0168@*1>
2017-09-26 16:03:55 +0800

    init

--- akka_demo_2.5.4/src/test/java/com/lightbend/akka/sample/AkkaQuickstartTest.java
+++ akka_demo_2.5.4/src/test/java/com/lightbend/akka/sample/AkkaQuickstartTest.java
@@ -3,14 +3,14 @@ package com.lightbend.akka.sample;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.testkit.javadsl.TestKit;
-import com.lightbend.akka.sample.Greeter.*;
-import com.lightbend.akka.sample.Printer.*;
-
-import static org.junit.Assert.assertEquals;
-
+import com.lightbend.akka.sample.Greeter.Greet;
+import com.lightbend.akka.sample.Greeter.WhoToGreet;
+import com.lightbend.akka.sample.Printer.Greeting;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class AkkaQuickstartTest {
     static ActorSystem system;
