e86c4e01331c3499949ae7f134c610935356ac2f
hzliubenlong <Lbl0168@*1>
2017-10-08 13:41:10 +0800

    init

--- akka_demo_2.5.4/src/main/java/com/lightbend/akka/path/Main.java
+++ akka_demo_2.5.4/src/main/java/com/lightbend/akka/path/Main.java
@@ -11,20 +11,14 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         final ActorSystem system = ActorSystem.create("akkaPath");
-        ActorRef printerActor11 = system.actorOf(Printer.props(), "printerActor11");
-        ActorRef printerActor22 = system.actorOf(Printer.props(), "printerActor22");
+        system.actorOf(Printer.props(), "printerActor11");
+        system.actorOf(Printer.props(), "printerActor22");
         ActorRef factory = system.actorOf(Factory.props(), "factory");
-
-        factory.tell(new Factory.FactoryMsg("hi tom "), ActorRef.noSender());
-
+        factory.tell(new Factory.FactoryMsg("hi factory "), ActorRef.noSender());
 
         ActorSelection actorSelection = system.actorSelection("akka://akkaPath/user/printerActor*");
         actorSelection.tell(new Printer.Greeting("haha"), ActorRef.noSender());
 
         factory.tell(new Factory.FactoryMsg("find"), ActorRef.noSender());
-
-
-//        TimeUnit.SECONDS.sleep(1);
-//        system.terminate();
     }
 }
