bef1b1d2572246c5b4f87c7e0eeb1bc62f7ce7b5
hzliubenlong <Lbl0168@*1>
2017-10-17 19:40:13 +0800

    init

--- akka_demo_2.5.4/src/main/java/com/lightbend/akka/demo/HotSwapActor.java
+++ akka_demo_2.5.4/src/main/java/com/lightbend/akka/demo/HotSwapActor.java
@@ -7,9 +7,12 @@ import akka.actor.Props;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
 
+/**
+ * 给小孩玩具就笑，拿走玩具就哭
+ */
 public class HotSwapActor extends AbstractActor {
-    private AbstractActor.Receive angry;
-    private AbstractActor.Receive happy;
+    private AbstractActor.Receive smileReceive;
+    private AbstractActor.Receive cryReceive;
 
     static public Props props() {
         return Props.create(HotSwapActor.class, () -> new HotSwapActor());
@@ -17,28 +20,44 @@ public class HotSwapActor extends AbstractActor {
 
     private LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
 
+    enum ToyStatus{
+        HAVETOY, NOTOY;
+    }
+
     public HotSwapActor() {
-        angry = receiveBuilder()
-                .matchEquals("foo", s -> log.info("I am already angry?.param={}", s))
-                .matchEquals("bar", s -> getContext().become(happy))
+        smileReceive = receiveBuilder()
+                .matchEquals(ToyStatus.HAVETOY, s -> log.info("我已经有玩具了，很开心"))
+                .matchEquals(ToyStatus.NOTOY, s -> {
+                    log.info("拿走了玩具。");
+                    getContext().become(cryReceive);
+                })
                 .build();
 
-        happy = receiveBuilder()
-                .matchEquals("bar", s -> log.info("I am already happy :-).param={}", s))
-                .matchEquals("foo", s -> getContext().become(angry))
+        cryReceive = receiveBuilder()
+                .matchEquals(ToyStatus.NOTOY, s -> log.info("没有玩具了，我要哭了"))
+                .matchEquals(ToyStatus.HAVETOY, s -> {
+                    log.info("拿到了玩具。");
+                    getContext().become(smileReceive);
+                })
                 .build();
     }
 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
-                .matchEquals("foo", s -> {
-                    log.info("收到foo消息，替换为angry。");
-                    getContext().become(angry);
+                .matchEquals(ToyStatus.NOTOY, s -> {
+                    log.info("拿走玩具。");
+                    getContext().become(cryReceive);
                 })
-                .matchEquals("bar", s -> {
-                    log.info("收到bar消息，替换为happy。");
-                    getContext().become(happy);
+                .matchEquals(ToyStatus.HAVETOY, s -> {
+                    log.info("给小孩玩具。");
+                    getContext().become(receiveBuilder()
+                            .matchEquals(ToyStatus.HAVETOY, o -> log.info("我已经有玩具了，很开心"))
+                            .matchEquals(ToyStatus.NOTOY, ss -> {
+                                log.info("拿走了玩具。");
+                                getContext().unbecome();
+                            })
+                            .build());
                 })
                 .build();
     }
@@ -47,9 +66,9 @@ public class HotSwapActor extends AbstractActor {
         ActorSystem system = ActorSystem.create("hotSwapAkka");
         ActorRef hotSwapActor = system.actorOf(HotSwapActor.props(), "hotSwapActor");
 
-        hotSwapActor.tell("foo", ActorRef.noSender());
-        hotSwapActor.tell("foo", ActorRef.noSender());
-        hotSwapActor.tell("bar", ActorRef.noSender());
-        hotSwapActor.tell("bar", ActorRef.noSender());
+        hotSwapActor.tell(ToyStatus.NOTOY, ActorRef.noSender());
+        hotSwapActor.tell(ToyStatus.NOTOY, ActorRef.noSender());
+        hotSwapActor.tell(ToyStatus.HAVETOY, ActorRef.noSender());
+        hotSwapActor.tell(ToyStatus.HAVETOY, ActorRef.noSender());
     }
 }
