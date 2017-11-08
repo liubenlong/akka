be0bbdadee4bc95426ab4a486c354a5ce1ebd18e
hzliubenlong <Lbl0168@*1>
2017-11-08 16:36:28 +0800

    init

--- akka_demo_2.5.4/src/main/java/com/lightbend/akka/mailbox/Demo.java
+++ akka_demo_2.5.4/src/main/java/com/lightbend/akka/mailbox/Demo.java
@@ -5,24 +5,13 @@ import akka.event.Logging;
 import akka.event.LoggingAdapter;
 import com.typesafe.config.ConfigFactory;
 
+import java.util.Arrays;
+
 class Demo extends AbstractActor {
     LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
 
     static public Props props() {
         return Props.create(Demo.class, () -> new Demo());
-    }
-
-    {
-//        for (Object msg : new Object[]{"lowpriority", "lowpriority",
-//                "highpriority", "pigdog", "pigdog2", "pigdog3", "highpriority"
-//                }) {
-//            getSelf().tell(msg, getSelf());
-//        }
-
-        for (Object msg : new Object[] { "foo", "bar", new MyControlMessage("123"), "tom",
-                PoisonPill.getInstance() }) {
-            getSelf().tell(msg, getSelf());
-        }
     }
 
     @Override
@@ -34,7 +23,15 @@ class Demo extends AbstractActor {
 
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("timerAkka", ConfigFactory.load("dev.conf"));
-        ActorRef myActor = system.actorOf(Demo.props().withDispatcher("akka.myUnboundedMailbox-dispatcher"), "demo");
+
+//        ActorRef myActor = system.actorOf(Demo.props().withDispatcher("akka.prio-dispatcher"), "demo");
+//        Arrays.asList("lowpriority", "lowpriority", "highpriority", "pigdog", "pigdog2", "pigdog3", "highpriority")
+//                .forEach(o -> myActor.tell(o, ActorRef.noSender()));
+
+
+        ActorRef myActor = system.actorOf(Demo.props().withDispatcher("akka.control-aware-dispatcher"), "demo");
+        Arrays.asList("foo", "bar", new MyControlMessage("123"), "tom", PoisonPill.getInstance())
+                .forEach(o -> myActor.tell(o, ActorRef.noSender()));
     }
 }
 
