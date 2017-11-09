3e0d0f13d104cc68ea16d616376f96f27c384fee
hzliubenlong <Lbl0168@*1>
2017-11-09 10:20:50 +0800

    init

--- akka_demo_2.5.4/src/main/java/com/lightbend/akka/route/PoolRouteActor.java
+++ akka_demo_2.5.4/src/main/java/com/lightbend/akka/route/PoolRouteActor.java
@@ -12,8 +12,10 @@ public class PoolRouteActor  {
 
     public static void main(String[] args) throws InterruptedException {
         ActorSystem system = ActorSystem.create("routeAkka", ConfigFactory.load("dev.conf"));
-        ActorRef actorRef = system.actorOf(new RoundRobinPool(3).props(Props.create(Printer.class)), "myRouterActor");
-//        ActorRef actorRef = system.actorOf(FromConfig.getInstance().props(Props.create(Printer.class)), "myRouterActor");
+        //方式一：通过代码实现
+//        ActorRef actorRef = system.actorOf(new RoundRobinPool(3).props(Props.create(Printer.class)), "myRouterActor");
+        //方式二：通过配置文件实现。注意这里后面的name就是配置文件中配置的名称
+        ActorRef actorRef = system.actorOf(FromConfig.getInstance().props(Props.create(Printer.class)), "myRouterActor");
 
         for (int i = 0; i < 7; i++) {
             actorRef.tell(new Printer.Greeting("msg" + i), ActorRef.noSender());
